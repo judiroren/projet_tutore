5ebc26c281a2441526dd1d8e8bcaf3a30a5bcbdf
julienDigot <jddu12@gmail.com>
2015-11-27 13:49:01 +0100

    Ajout des champs pour la gestion des employés.

--- projet_tutore/modif_employe.php
+++ projet_tutore/modif_employe.php
@@ -54,8 +54,50 @@ $nomE = $i->nomEntreprise;
 				<!-- Intro -->
 					
 						<div class="container">
-
-							
+							<h1>Gestion des employés de l'entreprise</h1>
+							<?php 
+							if(isset($_POST['verif'])){
+								echo "<p> Changement effectué </p>";
+							}
+							?>
+							<form method="post" action="">
+								<div class="row">
+								</br>
+									Nom de l'employé : <div class="6u 12u$(mobile)"><input type="text" name="mail" value="<?php echo $i->mailEntreprise?>" /></div>			
+									</br></br></br>
+									Prénom de l'employé: <div class="6u 12u$(mobile)"><input type="text" name="tel" value="<?php echo $i->telEntreprise?>"/></div>				
+									</br></br></br>
+									Adresse postale : <div class="6u 12u$(mobile)"><input type="text" name="adresse" value="<?php echo $i->adresseEntreprise?>"/></div>	
+									</br></br></br>
+									Adresse mail : <div class="6u 12u$(mobile)"><input type="text" name="logo" value="<?php echo $i->logoEntreprise?>"/></div>				
+									</br></br></br>
+									Numéro de téléphone : <div class="6u 12u$(mobile)"><input type="text" name="login" value="<?php echo $i->loginAdmin?>"/></div>				
+									</br></br></br>
+									Compétence 1 : <div class="6u 12u$(mobile)"><input type="text" name="mdp" value="<?php echo $i->mdpAdmin?>"/></div>								
+									</br></br></br>
+									Compétence 2 : <div class="6u 12u$(mobile)"><input type="text" name="mdp" value="<?php echo $i->mdpAdmin?>"/></div>								
+									</br></br></br>
+									Compétence 3 : <div class="6u 12u$(mobile)"><input type="text" name="mdp" value="<?php echo $i->mdpAdmin?>"/></div>								
+									</br></br></br>
+									
+								</div>
+								</br>
+									
+<table>
+<tr><td></td><td>Matin</td><td>Après-Midi</td></tr>
+<tr><td>Lundi</td><td><input type="checkbox" name="presence[]" value="LunM" /></td><td><input type="checkbox" name="presence[]" value="LunA" /></td></tr>
+<tr><td>Mardi</td><td><input type="checkbox" name="presence[]" value="MarM" /></td><td><input type="checkbox" name="presence[]" value="MarA" /></td></tr>
+<tr><td>Mercredi</td><td><input type="checkbox" name="presence[]" value="MerM" /></td><td><input type="checkbox" name="presence[]" value="MerA" /></td></tr>
+<tr><td>Jeudi</td><td><input type="checkbox" name="presence[]" value="JeuM" /></td><td><input type="checkbox" name="presence[]" value="JeuA" /></td></tr>
+<tr><td>Vendredi</td><td><input type="checkbox" name="presence[]" value="VenM" /></td><td><input type="checkbox" name="presence[]" value="VenA" /></td></tr>
+<tr><td>Samedi</td><td><input type="checkbox" name="presence[]" value="SamM" /></td><td><input type="checkbox" name="presence[]" value="SamA" /></td></tr>
+</table>
+								</br>
+								<input type="hidden" name="verif" value="ok"> 
+								<div align = "center" class="12u$">
+									<input type="submit" value="Ajouter" />
+								</div>
+							</form>
 						</div>
 
 						
