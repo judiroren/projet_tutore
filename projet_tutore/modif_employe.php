4766478bdd133b643d13357d58aba4d1025aac31
julienDigot <jddu12@gmail.com>
2015-11-28 15:42:59 +0100

    Ajout nouvel employé et planning ok. Quelques changement CSS.

--- projet_tutore/modif_employe.php
+++ projet_tutore/modif_employe.php
@@ -7,10 +7,23 @@ try {
 	echo 'Connexion échouée : ' . $e->getMessage();
 }
 
-$nom=$_GET['nomEntreprise'];
-$rqt = $connexion->query('SELECT * FROM entreprise WHERE nomEntreprise = "'.$nom.'"');
-$i = $rqt->fetch(PDO::FETCH_OBJ);
-$nomE = $i->nomEntreprise;
+$nomE=$_GET['nomEntreprise'];
+$rqt = $connexion->query("SELECT * FROM ".$nomE."_employe");
+$infoE = $connexion->query('SELECT * FROM entreprise WHERE nomEntreprise = "'.$nomE.'"');
+$i = $infoE->fetch(PDO::FETCH_OBJ);
+
+if(isset($_POST['ajout'])){
+	$connexion->exec("INSERT INTO ".$nomE."_employe(nom_employe, prenom_employe, adresse_emp, mail_emp, telephone_emp, competenceA, competenceB, competenceC) VALUES ('".$_POST['nom']."', '".$_POST['prenom']."', '".$_POST['adresse']."', '".$_POST['mail']."', '".$_POST['tel']."', '".$_POST['cmp1']."', '".$_POST['cmp2']."', '".$_POST['cmp3']."')");
+	$LundiM = (isset($_POST['LunM']) )? 1 : 0;		$LundiA = (isset($_POST['LunA']) )? 1 : 0;
+	$MardiM = (isset($_POST['MarM']) )? 1 : 0;		$MardiA = (isset($_POST['MarA']) )? 1 : 0;
+	$MercrediM = (isset($_POST['MerM']) )? 1 : 0;	$MercrediA = (isset($_POST['MerA']) )? 1 : 0;
+	$JeudiM = (isset($_POST['JeuM']) )? 1 : 0;		$JeusiA = (isset($_POST['JeuA']) )? 1 : 0;
+	$VendrediM = (isset($_POST['VenM']) )? 1 : 0;	$VendrediA = (isset($_POST['VenA']) )? 1 : 0;
+	$SamediM = (isset($_POST['SamM']) )? 1 : 0;		$SamediA = (isset($_POST['SamA']) )? 1 : 0;
+	$rqt2 = $connexion->query("SELECT id_employe FROM ".$nomE."_employe WHERE nom_employe = '".$_POST['nom']."'");
+	$idemp = $rqt2->fetch(PDO::FETCH_OBJ);
+	$connexion->exec("INSERT INTO ".$nomE."_planning(code_employe, LundiM, LundiA, MardiM, MardiA, MercrediM, MercrediA, JeudiM, JeudiA, VendrediM, VendrediA, SamediM, SamediA) VALUES ('".$idemp->id_employe."', ".$LundiM.", ".$LundiA.", ".$MardiM.", ".$MardiA.", ".$MercrediM.", ".$MercrediA.", ".$JeudiM.", ".$JeusiA.", ".$VendrediM.", ".$VendrediA.", ".$SamediM.", ".$SamediA.")");
+}
 ?>
 
 <html>
@@ -56,44 +69,63 @@ $nomE = $i->nomEntreprise;
 						<div class="container">
 							<h1>Gestion des employés de l'entreprise</h1>
 							<?php 
-							if(isset($_POST['verif'])){
-								echo "<p> Changement effectué </p>";
+							if(isset($_POST['ajout'])){
+								echo "<p> Ajout d'employé effectué </p>";
 							}
 							?>
+							<form method=post" action="">
+								<div class="6u 12u$(mobile)"><select name="employe_modif">
+								<?php 
+								$listeEmp = $connexion->query("SELECT id_employe, nom_employe, prenom_employe FROM ".$nomE."_employe");
+								while($donnees=$listeEmp->fetch(PDO::FETCH_OBJ)){
+									$identite = $donnees->nom_employe." ".$donnees->prenom_employe;
+								?>
+									<option value="<?php echo $donnees->id_employe ?>"><?php echo $identite; ?></option>   
+								<?php
+								}
+								?>
+								</select></div></br>
+								<div align = "center" class="12u$">
+									<input type="submit" value="Modifier" />
+									<input type="submit" value="Supprimer" />
+								</div>
+							</form>
+							</br>
+							<h2>Nouvel employe</h2>
 							<form method="post" action="">
 								<div class="row">
 								</br>
-									Nom de l'employé : <div class="6u 12u$(mobile)"><input type="text" name="mail" value="<?php echo $i->mailEntreprise?>" /></div>			
+									Nom de l'employé : <div class="6u 12u$(mobile)"><input type="text" name="nom"  /></div>			
 									</br></br></br>
-									Prénom de l'employé: <div class="6u 12u$(mobile)"><input type="text" name="tel" value="<?php echo $i->telEntreprise?>"/></div>				
+									Prénom de l'employé: <div class="6u 12u$(mobile)"><input type="text" name="prenom" /></div>				
 									</br></br></br>
-									Adresse postale : <div class="6u 12u$(mobile)"><input type="text" name="adresse" value="<?php echo $i->adresseEntreprise?>"/></div>	
+									Adresse postale : <div class="6u 12u$(mobile)"><input type="text" name="adresse" /></div>	
 									</br></br></br>
-									Adresse mail : <div class="6u 12u$(mobile)"><input type="text" name="logo" value="<?php echo $i->logoEntreprise?>"/></div>				
+									Adresse mail : <div class="6u 12u$(mobile)"><input type="text" name="mail" /></div>				
 									</br></br></br>
-									Numéro de téléphone : <div class="6u 12u$(mobile)"><input type="text" name="login" value="<?php echo $i->loginAdmin?>"/></div>				
+									Numéro de téléphone : <div class="6u 12u$(mobile)"><input type="text" name="tel" /></div>				
 									</br></br></br>
-									Compétence 1 : <div class="6u 12u$(mobile)"><input type="text" name="mdp" value="<?php echo $i->mdpAdmin?>"/></div>								
+									Compétence 1 : <div class="6u 12u$(mobile)"><input type="text" name="cmp1" /></div>								
 									</br></br></br>
-									Compétence 2 : <div class="6u 12u$(mobile)"><input type="text" name="mdp" value="<?php echo $i->mdpAdmin?>"/></div>								
+									Compétence 2 : <div class="6u 12u$(mobile)"><input type="text" name="cmp2" /></div>								
 									</br></br></br>
-									Compétence 3 : <div class="6u 12u$(mobile)"><input type="text" name="mdp" value="<?php echo $i->mdpAdmin?>"/></div>								
+									Compétence 3 : <div class="6u 12u$(mobile)"><input type="text" name="cmp3" /></div>								
 									</br></br></br>
 									
 								</div>
 								</br>
 									
-<table>
-<tr><td></td><td>Matin</td><td>Après-Midi</td></tr>
-<tr><td>Lundi</td><td><input type="checkbox" name="presence[]" value="LunM" /></td><td><input type="checkbox" name="presence[]" value="LunA" /></td></tr>
-<tr><td>Mardi</td><td><input type="checkbox" name="presence[]" value="MarM" /></td><td><input type="checkbox" name="presence[]" value="MarA" /></td></tr>
-<tr><td>Mercredi</td><td><input type="checkbox" name="presence[]" value="MerM" /></td><td><input type="checkbox" name="presence[]" value="MerA" /></td></tr>
-<tr><td>Jeudi</td><td><input type="checkbox" name="presence[]" value="JeuM" /></td><td><input type="checkbox" name="presence[]" value="JeuA" /></td></tr>
-<tr><td>Vendredi</td><td><input type="checkbox" name="presence[]" value="VenM" /></td><td><input type="checkbox" name="presence[]" value="VenA" /></td></tr>
-<tr><td>Samedi</td><td><input type="checkbox" name="presence[]" value="SamM" /></td><td><input type="checkbox" name="presence[]" value="SamA" /></td></tr>
-</table>
+								<table>
+									<tr><td></td><td>Matin</td><td>Après-Midi</td></tr>
+									<tr><td>Lundi</td><td><input type="checkbox" name="LunM" value=1 /></td><td><input type="checkbox" name="LunA" value=1 /></td></tr>
+									<tr><td>Mardi</td><td><input type="checkbox" name="MarM" value=1 /></td><td><input type="checkbox" name="MarA" value=1 /></td></tr>
+									<tr><td>Mercredi</td><td><input type="checkbox" name="MerM" value=1 /></td><td><input type="checkbox" name="MerA" value=1 /></td></tr>
+									<tr><td>Jeudi</td><td><input type="checkbox" name="JeuM" value=1 /></td><td><input type="checkbox" name="JeuA" value=1 /></td></tr>
+									<tr><td>Vendredi</td><td><input type="checkbox" name="VenM" value=1 /></td><td><input type="checkbox" name="VenA" value=1 /></td></tr>
+									<tr><td>Samedi</td><td><input type="checkbox" name="SamM" value=1 /></td><td><input type="checkbox" name="SamA" value=1/></td></tr>
+								</table>
 								</br>
-								<input type="hidden" name="verif" value="ok"> 
+								<input type="hidden" name="ajout" value="ok"> 
 								<div align = "center" class="12u$">
 									<input type="submit" value="Ajouter" />
 								</div>
