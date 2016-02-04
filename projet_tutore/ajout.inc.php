4958106c29adf46de6ede0a42ac6f1858c6bffde
julienDigot <jddu12@gmail.com>
2016-02-04 14:02:46 +0100

    Ajout des fichiers de fonctions

---
+++ projet_tutore/ajout.inc.php
@@ -0,0 +1,240 @@
+<?php
+
+//Permet d'ajouter un employé
+function ajoutEmploye($connexion, $code, $nom, $prenom, $adresse, $mail, $tel, $presta1, $presta2, $presta3) {
+	
+	$nomE = $_SESSION["nomSession"];
+
+	$rqtAjoutEmp = $connexion->prepare("INSERT INTO ".$nomE."_employe(id_employe, nom_employe, prenom_employe, adresse_emp, 
+						mail_emp, telephone_emp, competenceA, competenceB, competenceC) 
+						VALUES (:code, :nom, :prenom, :adresse, :mail, :tel, :presta1, :presta2, :presta3)");
+						
+	$rqtAjoutEmp->execute(array("code" => $code, "nom" => $nom, "prenom" => $prenom, "adresse" => $adresse, 
+								"mail" => $mail, "tel" => $tel, "presta1" => $presta1, "presta2" => $presta2, "presta3" => $presta3));	
+								
+}
+
+//Permet de modifier un employé
+function majEmploye($connexion, $nom, $prenom, $adresse, $mail, $tel, $presta1, $presta2, $presta3, $id_employe) {
+	
+	$nomE = $_SESSION["nomE"];
+	$modifInfosEmp = $connexion->prepare("UPDATE ".$nomE."_employe SET nom_employe = :nom, 
+								prenom_employe = :prenom, adresse_emp = :adresse, 
+								mail_emp = :mail, telephone_emp = :tel, competenceA = :presta1,
+								competenceB = :presta2, competenceC = :presta3 WHERE id_employe = :id_employe");
+								
+	$modifInfosEmp->execute(array('nom' => $nom, 'prenom' => $prenom, 'adresse' => $adresse, 'mail' => $mail, 'tel' => $tel, 'presta1' => $presta1,
+									'presta2' => $presta2, 'presta3' => $presta3, 'id_employe' => $id_employe));	
+	
+}
+
+//Permet d'ajouter le planning d'un employé
+function ajoutPlanning($connexion, $code, $IDemp, $LundiM, $LundiA, $MardiM, $MardiA, $MercrediM, $MercrediA, $JeudiM, $JeudiA, $VendrediM, $VendrediA, $SamediM, $SamediA) {
+	
+	$nomE = $_SESSION["nomSession"];
+
+	$rqtAjoutPlan = $connexion->prepare("INSERT INTO ".$nomE."_planning(id_agenda, code_employe, LundiM, LundiA, MardiM, 
+													MardiA, MercrediM, MercrediA, JeudiM, JeudiA, VendrediM, VendrediA, SamediM, SamediA) 
+													VALUES (:code, :IDemp, :LundiM, :LundiA, :MardiM, 
+													:MardiA, :MercrediM, :$MercrediA, :JeudiM, :JeudiA, :VendrediM, 
+													:VendrediA, :SamediM, :SamediA)");
+													
+	$rqtAjoutPlan->execute(array("code" => $code, "IDemp" => $IDemp, "LundiM" => $LundiM, "LundiA" => $LundiA, "MardiM" => $MardiM, "MardiA" => $MardiA,
+								"MercrediM" => $MercrediM, "MercrediA" => $MercrediA, "JeudiM" => $JeudiM, "JeudiA" => $JeudiA, "VendrediM" => $VendrediM, 
+								"VendrediA" => $VendrediA, "SamediM" => $SamediM, "SamediA" => $SamediA));
+		
+}
+
+//Permet de modifer le planning d'un employé
+function majPlanning($connexion, $LundiM, $LundiA, $MardiM, $MardiA, $MercrediM, $MercrediA, $JeudiM, $JeudiA, $VendrediM, $VendrediA, $SamediM, $SamediA, $id_employe) {
+	
+	$nomE = $_SESSION["nomE"];
+	$modifPlan = $connexion->prepare("UPDATE LundiM = :LundiM, LundiA = :LundiA, 
+								MardiM = :MardiM, MardiA = :MardiA, MercrediM = :MercrediM, MercrediA = :MercrediA,
+								JeudiM = :JeudiM, JeudiA = :JeudiA, VendrediM = :VendrediM, VendrediA = :VendrediA,
+								SamediM = :SamediM, SamediA = :SamediA WHERE id_employe = :id_employe");
+								
+	$modifPlan->execute(array("LundiM" => $LundiM, "LundiA" => $LundiA, "MardiM" => $MardiM, "MardiA" => $MardiA,
+								"MercrediM" => $MercrediM, "MercrediA" => $MercrediA, "JeudiM" => $JeudiM, "JeudiA" => $JeudiA, "VendrediM" => $VendrediM, 
+								"VendrediA" => $VendrediA, "SamediM" => $SamediM, "SamediA" => $SamediA, 'id_employe' => $id_employe));	
+	
+}
+
+//Supprime un employé, dont son planning
+function supprimerEmp($connexion, $IDemp) {
+	
+	$nomE = $_SESSION["nomSession"];
+	
+	$rqtSupEmp = $connexion->prepare("DELETE FROM ".$nomE."_planning WHERE code_employe='".$IDemp."'");
+	$rqtSupEmp->execute();
+	
+}
+
+//Supprime le planning d'un employé
+function supprimerPlan($connexion, $IDemp) {
+	
+	$nomE = $_SESSION["nomSession"];
+	
+	$rqtSupPlan = $connexion->prepare("DELETE FROM ".$nomE."_employe WHERE id_employe='".$IDemp."'");
+	$rqtSupPlan->execute();
+}
+
+//Permet de créer une entreprise
+function creerEntreprise($connexion, $entreprise, $mail, $login, $mdpHash) {
+	
+	$rqtCreerEnt = $connexion->prepare("INSERT INTO entreprise(nomEntreprise, mailEntreprise, loginAdmin, mdpAdmin) 
+						VALUES (:entreprise, :mail, :login, :mdpHash)");
+						
+	$rqtCreerEnt->execute(array("entreprise" => $entreprise, "mail" => $mail, "login" => $login, "mdpHash" => $mdpHash));					
+		   
+}
+
+//Permet d'ajouter toutes les informations d'une entreprise
+function ajoutEntreprise($connexion, $temploye, $tprestation, $tclient, $treserv, $tplanning, $tabsence) {
+	
+	$rqtAjout1 = $connexion->prepare("CREATE TABLE ".$temploye." ( id_employe CHAR(8) PRIMARY KEY, nom_employe VARCHAR(40), 
+													prenom_employe VARCHAR(50), competenceA CHAR(8), competenceB CHAR(8), 
+													competenceC CHAR(8), telephone_emp CHAR(10), adresse_emp VARCHAR(200),
+													mail_emp VARCHAR(50))");
+	$rqtAjout1->execute();												
+													
+	$rqtAjout2 = $connexion->prepare("CREATE TABLE ".$tprestation." ( id_presta CHAR(8) PRIMARY KEY, descriptif_presta TEXT, prix DECIMAL(5,2), 
+														paypal BOOLEAN, duree INT)");
+	$rqtAjout2->execute();													
+														
+	$rqtAjout3 = $connexion->prepare("CREATE TABLE ".$tclient." ( id_client CHAR(8) PRIMARY KEY, nom_client VARCHAR(40), prenom_client VARCHAR(50), 
+													mail VARCHAR(50), login_client VARCHAR(30), mdp_client VARCHAR(30))");
+	$rqtAjout3->execute();												
+													
+	$rqtAjout4 = $connexion->prepare("CREATE TABLE ".$treserv." ( id_reserv CHAR(8) PRIMARY KEY, client CHAR(8), employe CHAR(8), presta CHAR(8), 
+													paye BOOLEAN, date DATE, heure TIME)");
+	$rqtAjout4->execute();												
+													
+	$rqtAjout5 = $connexion->prepare("CREATE TABLE ".$tplanning." (
+  					`id_agenda` char(8) PRIMARY KEY,
+					`code_employe` char(8) NOT NULL,
+ 					`LundiM` BOOLEAN, `LundiA` BOOLEAN,
+  					`MardiM` BOOLEAN, `MardiA` BOOLEAN,
+  					`MercrediM` BOOLEAN, `MercrediA` BOOLEAN,
+  					`JeudiM` BOOLEAN, `JeudiA` BOOLEAN,
+  					`VendrediM` BOOLEAN, `VendrediA` BOOLEAN,
+  					`SamediM` BOOLEAN, `SamediA` BOOLEAN)");
+	$rqtAjout5->execute();				
+					
+	$rqtAjout6 = $connexion->prepare("CREATE TABLE ".$tabsence." (
+  						`id_absence` char(8) PRIMARY KEY,
+						`code_employe` char(8) NOT NULL,
+ 						`motif` varchar(100),
+  						`dateDebut` date,
+  						`dateFin` date,
+  						`absenceFini` BOOLEAN)");
+	$rqtAjout6->execute();					
+}
+
+//Permet de modifier toutes les informations d'une entreprise
+function modifEntreprise($connexion, $mail, $tel, $adresse, $logo, $descrip, $login, $mdp) {
+	
+	$nomE = $_SESSION["nomE"];
+	/* $modifInfosEnt = $connexion->prepare("UPDATE entreprise SET mailEntreprise = '".$mail."', telEntreprise = '".$tel."', 
+								adresseEntreprise = '".$adresse."', logoEntreprise = '".$logo."', 
+								descEntreprise = '".$descrip."', loginAdmin = '".$login."', mdpAdmin = '".$mdp."' 
+								WHERE nomEntreprise = '".$nomE."'"); */
+								
+	$modifInfosEnt = $connexion->prepare("UPDATE entreprise SET mailEntreprise = :mail, telEntreprise = :tel, 
+								adresseEntreprise = :adresse, logoEntreprise = :logo, 
+								descEntreprise = :descrip, loginAdmin = :login, mdpAdmin = :mdp WHERE nomEntreprise = :nomE");						
+								
+	$modifInfosEnt->execute(array('mail' => $mail, 'tel' => $tel, 'adresse' => $adresse, 'logo' => $logo, 'descrip' => $descrip,
+									'login' => $login, 'mdp' => $mdp, 'nomE' => $nomE));							
+	
+}
+
+//Permet de modfier les informations d'une entreprise sauf le mot de passe
+function modifEnt($connexion, $mail, $tel, $adresse, $logo, $descrip, $login) {
+	
+	$nomE = $_SESSION["nomE"];
+	/** $modifInfosEnt2 = $connexion->prepare("UPDATE entreprise SET mailEntreprise = '".$mail."', telEntreprise = '".$tel."', 
+						adresseEntreprise = '".$adresse."', logoEntreprise = '".$logo."', 
+						descEntreprise = '".$descrip."', loginAdmin = '".$login."' WHERE nomEntreprise = '".$nomE."'"); */
+						
+	$modifInfosEnt2 = $connexion->prepare("UPDATE entreprise SET mailEntreprise = :mail, telEntreprise = :tel, 
+								adresseEntreprise = :adresse, logoEntreprise = :logo, 
+								descEntreprise = :descrip, loginAdmin = :login WHERE nomEntreprise = :nomE");						
+						
+	$modifInfosEnt2->execute(array('mail' => $mail, 'tel' => $tel, 'adresse' => $adresse, 'logo' => $logo, 'descrip' => $descrip,
+									'login' => $login, 'nomE' => $nomE));					
+	
+}
+
+//Permet d'ajouter une prestation
+function ajoutPresta($connexion, $code, $descrip, $prix, $paypal, $duree) {
+	
+	$nomE = $_SESSION["nomE"];
+	$rqtAjoutPresta = $connexion->prepare("INSERT INTO ".$nomE."_prestation(id_presta, descriptif_presta, prix, paypal, duree) 
+										VALUES (:code, :descrip, :prix, :paypal, :duree)");
+	$rqtAjoutPresta->execute(array('code' => $code, 'descrip' => $descrip, 'prix' => $prix, 'paypal' => $paypal, 'duree' => $duree));									
+	
+}
+
+//Permet de modifier une prestation
+function majPresta($connexion, $descrip, $prix, $paypal, $duree, $id_presta) {
+	
+	$nomE = $_SESSION["nomE"];
+	$modifPresta = $connexion->prepare("UPDATE ".$nomE."_prestation SET descriptif_presta = :descrip, 
+								prix = :prix, paypal = :paypal, duree = :duree WHERE id_presta = :id_presta");
+								
+	$modifPresta->execute(array('descrip' => $descrip, 'prix' => $prix, 'paypal' => $paypal, 'duree' => $duree, 'id_presta' => $id_presta));
+	
+}
+
+//Permet de mettre à jour la compétence A d'un employé
+function majCompA($connexion, $code, $employe) {
+	
+	$nomE = $_SESSION["nomE"];
+	$rqtMajComA = $connexion->prepare("UPDATE ".$nomE."_employe SET competenceA = :code WHERE id_employe = :employe");
+	$rqtMajComA->execute(array('code' => $code, 'employe' => $employe));
+}
+
+//Permet de mettre à jour la compétence B d'un employé
+function majCompB($connexion, $code, $employe) {
+	
+	$nomE = $_SESSION["nomE"];
+	$rqtMajComB = $connexion->prepare("UPDATE ".$nomE."_employe SET competenceB = :code WHERE id_employe = :employe");
+	$rqtMajComB->execute(array('code' => $code, 'employe' => $employe));
+}
+
+//Permet de mettre à jour la compétence C d'un employé
+function majCompC($connexion, $code, $employe) {
+	
+	$nomE = $_SESSION["nomE"];
+	$rqtMajComC = $connexion->prepare("UPDATE ".$nomE."_employe SET competenceC = :code WHERE id_employe = :employe");
+	$rqtMajComC->execute(array('code' => $code, 'employe' => $employe));
+}
+
+//Permet de supprimer une prestation
+function supprimerPresta($connexion, $presta) {
+	
+	$nomE = $_SESSION["nomSession"];
+	
+	$rqtSupPresta = $connexion->prepare("DELETE FROM ".$nomE."_prestation WHERE id_presta='".$presta."'");
+	$rqtSupPresta->execute();
+	
+}
+
+//Permet d'ajouter une réservation
+function ajoutReservation($connexion, $code, $employeAbsent, $motif, $debutReserv, $finReserv, $fin) {
+	
+	$nomE = $_SESSION["nomSession"];
+	
+	/**$connexion->exec("INSERT INTO ".$nomE."_absence(id_absence, code_employe, motif, dateDebut, dateFin, absenceFini) 
+					VALUES ('".$code."', '".$_POST['employe_absent']."', '".$_POST['motif']."', '".$_POST['debut']."', '".$_POST['fin']."', '".$fin."')"); */
+					
+	$rqtAjoutReserv = $connexion->prepare("INSERT INTO ".$nomE."_absence(id_absence, code_employe, motif, dateDebut, dateFin, absenceFini) 
+					VALUES (:code, :employeAbsent, :motif, :debutReserv, :finReserv, :fin)");
+
+	$rqtAjoutReserv->execute(array('code' => $code, 'employeAbsent' => $employeAbsent, 'motif' => $motif, 'debutReserv' => $debutReserv, 
+									'finReserv' => $finReserv, 'fin' => $fin));
+		
+}
+
+?>
---
+++ projet_tutore/bd.inc.php
@@ -0,0 +1,217 @@
+<?php
+
+//Permet la connection à la base de données
+function connect() {
+	try {
+		
+		$connexion = new PDO("mysql:dbname=portail_reserv;host=localhost", "geo_admin", "Admin%7" );
+		$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $e) {
+		
+		echo 'Connexion échouée : ' . $e->getMessage();
+	}
+	return $connexion;
+} 
+
+//Permet de récupérer les identifiants d'un administrateur
+function infosEntreprise() {
+	
+	$connexion = connect();
+	$nomE = $_GET['nomEntreprise'];
+	$rqt = $connexion->prepare('SELECT * FROM entreprise WHERE nomEntreprise = "' .$nomE. '"');
+	$rqt->execute();
+	$rqt->setFetchMode(PDO::FETCH_OBJ);
+	$i = $rqt->fetch(); 
+	
+	return $i;
+}
+
+//Permet de récupérer le planning d'un employé
+function planningEmp($id_employe) {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$planning = $connexion->prepare('SELECT * FROM '.$nomE.'_planning JOIN '.$nomE.'_employe ON id_employe = "'.$id_employe.'"');
+	
+	$planning->execute();
+	
+	return $planning;	
+	
+}
+
+//Permet de récupérer le planning d'une entreprise
+function planningEnt() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	//$planning = $connexion->prepare('SELECT * FROM '.$nomE.'_planning');
+	$planning = $connexion->prepare('SELECT * FROM '.$nomE.'_planning JOIN '.$nomE.'_employe ON code_employe = id_employe');
+	
+	$planning->execute();
+	
+	return $planning;	
+	
+}
+
+//Permet de récupérer les réservations
+function reservationsEnt() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$reserva = $connexion->prepare('SELECT * FROM '.$nomE.'_reserv 
+									JOIN '.$nomE.'_employe ON employe = id_employe 
+									JOIN '.$nomE.'_client ON client = id_client 
+									JOIN '.$nomE.'_prestation ON presta = id_presta');
+	$reserva->execute();
+	return $reserva;	
+									
+	
+}
+
+//Permet de récupérer les abscences des employés
+function abscencesEnt() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$absences = $connexion->prepare('SELECT * FROM '.$nomE.'_absence 
+									JOIN '.$nomE.'_employe ON code_employe = id_employe');
+	$absences->execute();
+	return $absences;	
+}
+
+//Permet de récupérer les abscences d'une entreprise
+function listeAbscences() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+
+	$absences = $connexion->prepare('SELECT id_absence FROM '.$nomE.'_absence');
+	$absences->execute();
+	
+	return $absences;	
+}
+
+//Permet de récupérer les prestations d'une entreprise
+function listePrestations() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$rqtPrestations = $connexion->prepare("SELECT id_presta, descriptif_presta FROM ".$nomE."_prestation");
+	$rqtPrestations->execute();
+	
+	return $rqtPrestations;
+}
+
+//Permet d'obtenir les informations sur les planning d'une entreprise
+function listePlanning() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$rqtPlanning = $connexion->prepare("SELECT * FROM ".$nomE."_planning");
+	$rqtPlanning->execute();
+	
+	return $rqtPlanning;
+	
+}
+
+//Permet d'obtenir les informations d'une prestation précise 
+function infosPrestation($id) {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$listePresta = $connexion->prepare("SELECT * FROM ".$nomE."_prestation WHERE id_presta = '".$id."'");
+	$listePresta->execute();
+	
+	return $listePresta;
+}
+
+//Permet de récupérer les informations des employés d'une entreprise
+function infosEmploye() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$rqtInfosEmp = $connexion->prepare("SELECT * FROM ".$nomE."_employe");
+	$rqtInfosEmp->execute();
+	
+	return $rqtInfosEmp;
+}
+
+//Permet d'obtenir les informations pour vérifier un employé
+function verifEntreprise($nomE) {
+	
+	
+	$connexion = connect();
+	$rqtVerifEnt = $connexion->prepare('SELECT nomEntreprise FROM entreprise WHERE nomEntreprise = "' .$nomE. '"');
+	$rqtVerifEnt->execute();
+	
+	$rqtVerifEnt->setFetchMode(PDO::FETCH_ASSOC);
+	$tableau = null;
+	$tableau = $rqtVerifEnt->fetch();
+	return $tableau;
+	
+}
+
+//Permet d'obtenir les informations pour vérifier les employés d'une entreprise
+function verifEmploye() {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$rqtVerifEmp = $connexion->prepare("SELECT nom_employe, prenom_employe FROM ".$nomE."_employe");
+	$rqtVerifEmp->execute();
+	
+	return $rqtVerifEmp;
+}
+
+//Permet d'obtenir l'Id d'un employé
+function IdentEmploye($nomEmp) {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	//$rqtIdEmp = $connexion->prepare("SELECT id_employe FROM ".$nomE."_employe WHERE nom_employe = '".$nomEmp."'");
+	$rqtIdEmp = $connexion->prepare("SELECT id_employe FROM ".$nomE."_employe WHERE nom_employe = '".$nomEmp."'");
+	$rqtIdEmp->execute();
+	
+	//$rqtIdEmp->fetch(PDO::FETCH_OBJ);
+	//$i = $rqtIdEmp->fetch();
+	
+	return $rqtIdEmp;
+} 
+
+//Permet d'obtenir toutes les informations d'un employé
+function InfosEmploye2($nomEmp) {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$rqtIdEmp = $connexion->prepare("SELECT * FROM ".$nomE."_employe WHERE id_employe = '".$nomEmp."'");
+	$rqtIdEmp->execute();
+	
+	return $rqtIdEmp;
+	
+}
+
+//Permet de séléctionner les réservations d'un employé
+function reservationsEmp($idemp) {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	//$rqt = $connexion->query('SELECT * FROM '.$nomE.'_reserv WHERE employe = "'.$_POST['employe_modif'].'"');
+	$rqtReservEmp = $connexion->prepare('SELECT * FROM '.$nomE.'_reserv WHERE employe = ":idemp"');
+	$rqtReservEmp->execute(array('idemp' => $idemp));
+	
+	return $rqtReservEmp;
+	
+}
+
+//Permet de sélectionner la date d'une reservation précise
+function dateReservation($employeAbsent, $debutReserv, $finReserv) {
+	
+	$connexion = connect();
+	$nomE = $_SESSION["nomE"];
+	$reserv = $connexion->query('SELECT date FROM '.$nomE.'_reserv WHERE employe = "'.$employeAbsent.'" 
+								AND date BETWEEN '.$debutReserv.' AND '.$finReserv);
+	$reserv->execute();
+
+	return $reserv;	
+}
+
+?>
